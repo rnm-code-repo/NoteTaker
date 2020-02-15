e685b13621d9761d9e71630d263c189a3abd00ad
Rekha Ballur <rnmadalli@gmail.com>
2020-02-15 08:37:07 -0500

    Initial checkin

---
+++ Develop/app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const path = require("path");
+const fs = require('fs');
+
+const app = express()
+const port = 8080
+app.use(express.static("public"));
+
+let fileContent = fs.readFileSync("./db/db.json", "utf-8");
+fileContent ? fileContent = JSON.parse(fileContent) : fileContent = [];
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// Default route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// Route to show notes main page
+app.get('/notes', (req, res) => {
+    return res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// API end points
+app.get('/api/notes', (req, res) => {
+    return res.send(fileContent);
+})
+
+app.post('/api/notes', (req, res) => {
+    var id = fileContent[fileContent["length"] - 1].id === undefined ? 1 : fileContent[fileContent["length"] - 1].id + 1;
+
+    fileContent.push({
+        "id": id,
+        ...req.body
+    });
+    fs.writeFileSync("./db/db.json", JSON.stringify(fileContent), "utf-8");
+    res.json(true);
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    const delId = fileContent.find(note => note.id === parseInt(req.params.id));
+
+    if (!delId) return res.status(404).send("The note you are deleting is not available");
+    const index = fileContent.indexOf(delId);
+
+    fileContent.splice(index, 1);
+    fs.writeFileSync("./db/db.json", JSON.stringify(fileContent), "utf-8");
+    return res.json(true);
+});
+app.listen(port, () => console.log(`NotesTaker App listening on port ${port}!`))
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # NoteTaker
-Simple application to demo Express rest calls
+---
+##Simple application to demo REST API calls
+
+This Node/Express JS application gives an overview of REST API calls
+- GET - `route - /api/notes` Reads data from a JSON file
+- POST - `route - /api/notes`  Writes data coming from form to a JSON file
+- DELETE - `route - /api/notes/:id` Delets data with given id from JSON file
+
+---
